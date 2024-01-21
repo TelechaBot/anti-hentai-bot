dfc60e5d1ec16cbb0b89f03de6b89c23312d38e6
sudoskys <coldlando@hotmail.com>
2024-01-21 13:18:23 +0800

    :sparkles: feat(controller): Add photo and document handling in group chats
    - Added logic to handle photo and document messages in group chats.
    - Implemented image censorship using the pipeline_pass function.
    - If an image is detected as potentially illegal, the bot will delete the message and warn the user.
    - Added a command /report to report spam messages and perform image censorship on the replied message.
    - Implemented checks for settings.rules and settings.mode to determine whether to perform censorship.

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,7 @@
 # @Time    : 2023/11/18 上午12:18
 # @File    : controller.py
 # @Software: PyCharm
+from io import BytesIO
 
 from asgiref.sync import sync_to_async
 from loguru import logger
@@ -11,6 +12,7 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_helper import ApiTelegramException
 from telebot.asyncio_storage import StateMemoryStorage
 
+from app.event import pipeline_pass
 from app_conf import settings
 from setting.telegrambot import BotSetting
 
@@ -25,6 +27,33 @@ def sync_to_async_func():
 class BotRunner(object):
     def __init__(self):
         self.bot = AsyncTeleBot(BotSetting.token, state_storage=StepCache)
+
+    async def download(self, file):
+        assert hasattr(file, "file_id"), "file_id not found"
+        name = file.file_id
+        _file_info = await self.bot.get_file(file.file_id)
+        if isinstance(file, types.PhotoSize):
+            name = f"{_file_info.file_unique_id}.jpg"
+        if isinstance(file, types.Document):
+            name = f"{file.file_unique_id} {file.file_name}"
+        if not name.endswith(("jpg", "png", "webp")):
+            return None
+        downloaded_file = await self.bot.download_file(_file_info.file_path)
+        return downloaded_file
+
+    async def censor(self, file):
+        file_data = await self.download(file=file)
+        if file_data is None:
+            return False, "🥛 Not An image"
+        if isinstance(file_data, bytes):
+            file_data = BytesIO(file_data)
+        result = await pipeline_pass(trace_id="test", content=file_data)
+        if result.risk_tag:
+            return True, f"🔨 Image Content Maybe illegal: {result.risk_tag}"
+        return (
+            False,
+            f"🥽 AnimeScore {result.anime_score}\n☕️ Content {result.anime_tags[:7]}",
+        )
 
     async def run(self):
         logger.info("Bot Start")
@@ -50,13 +79,68 @@ class BotRunner(object):
                 parse_mode="MarkdownV2",
             )
 
+        async def delete_or_warn(message: types.Message, reason: str):
+            reason_detail = f"For User {message.from_user.last_name}[{message.from_user.id}]\n{reason}"
+            try:
+                await self.bot.delete_message(
+                    chat_id=message.chat.id, message_id=message.message_id
+                )
+            except Exception as er:
+                logger.exception(er)
+            try:
+                await self.bot.reply_to(message=message, text=reason_detail)
+            except Exception as er:
+                logger.exception(er)
+            return True
+
         @bot.message_handler(
             content_types=["photo", "document"], chat_types=["supergroup", "group"]
         )
         async def handle_group_photo(message: types.Message):
+            if settings.rule.only_report:
+                return logger.debug("Only Report So ignore")
+            if settings.mode.only_white:
+                if message.chat.id not in settings.mode.white_group:
+                    return logger.info(f"White List Out {message.chat.id}")
             if not settings.rule.check_spoiler_photo:
                 if message.has_media_spoiler:
                     return logger.info("Ignore Spoiler Photos")
+            message_ph = message.photo
+            message_doc = message.document
+            if message_ph:
+                filtered, reason = await self.censor(file=message_ph[-1])
+                if filtered:
+                    await delete_or_warn(message=message, reason=reason)
+            if message_doc:
+                filtered, reason = await self.censor(file=message_doc)
+                if filtered:
+                    await delete_or_warn(message=message, reason=reason)
+
+        @bot.message_handler(commands="report", chat_types=["supergroup", "group"])
+        async def report(message: types.Message):
+            if settings.mode.only_white:
+                if message.chat.id not in settings.mode.white_group:
+                    return logger.info(f"White List Out {message.chat.id}")
+            if not message.reply_to_message:
+                return await bot.reply_to(
+                    message, text="🍡 please reply to spam message with this command"
+                )
+            reply_message = message.reply_to_message
+            reply_message_ph = reply_message.photo
+            reply_message_doc = reply_message.document
+            if reply_message_ph:
+                filtered, reason = await self.censor(file=reply_message_ph[-1])
+                if filtered:
+                    return await delete_or_warn(message=reply_message, reason=reason)
+                else:
+                    return await bot.reply_to(message, text=reason)
+            if reply_message_doc:
+                filtered, reason = await self.censor(file=reply_message_doc)
+                if filtered:
+                    return await delete_or_warn(message=reply_message, reason=reason)
+                else:
+                    return await bot.reply_to(message, text=reason)
+            return await bot.reply_to(message, text="🥛 Not image")
 
         try:
             await bot.polling(
--- app/event.py
+++ app/event.py
@@ -3,3 +3,56 @@
 # @Author  : sudoskys
 # @File    : event.py
 # @Software: PyCharm
+from io import TextIOBase
+from typing import Union, IO, List, Optional
+
+from anime_identify import AnimeIDF
+from loguru import logger
+from pydantic import BaseModel
+
+from app.utils import WdTaggerSDK
+from setting.wdtagger import TaggerSetting
+
+ANIME = AnimeIDF()
+
+
+class CensorResult(BaseModel):
+    anime_score: float
+    risk_tag: Optional[List[str]] = []
+    anime_tags: Optional[List[str]] = []
+
+
+def build_risk_tag(tags):
+    _risk = []
+    if "loli" in tags:
+        _risk.append("loli")
+    if "6+girls" in tags:
+        _risk.append("6+girls")
+    if "multiple_girls" in tags:
+        _risk.append("multiple_girls")
+    if "censored" in tags:
+        _risk.append("censored")
+    return _risk
+
+
+async def pipeline_pass(trace_id, content: Union[IO, TextIOBase]) -> CensorResult:
+    content.seek(0)
+    anime_score = ANIME.predict_image(content=content)
+    if anime_score > 50:
+        logger.info(f"Censored {trace_id},score {anime_score}")
+        return CensorResult(anime_score=anime_score)
+    content.seek(0)
+    raw_output_wd = await WdTaggerSDK(base_url=TaggerSetting.wd_api_endpoint).upload(
+        file=content.read(),
+        token="censor",
+        general_threshold=0.5,
+        character_threshold=0.85,
+    )
+    content.close()
+    tag_result: dict = raw_output_wd["general_res"]
+    risk_tag = build_risk_tag(tags=tag_result)
+    anime_tag = list(tag_result.keys())
+    logger.info(f"Censored {trace_id},score {anime_score},result {tag_result}")
+    return CensorResult(
+        anime_score=anime_score, risk_tag=risk_tag, anime_tags=anime_tag
+    )
